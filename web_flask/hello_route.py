7b651ee660087fadc9311c587e1036d425720827
kingvadee <ojeifod8@gmail.com>
2023-12-01 00:40:35 +0100

    updated files

--- web_flask/hello_route.py
+++ web_flask/hello_route.py
@@ -3,14 +3,14 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
+    /airbnb-onepage/: Displays 'Hello HBNB!'
 """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
@@ -18,3 +18,4 @@ def hello_hbnb():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+
