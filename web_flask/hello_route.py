cf53822bbec333a610792659d3e5e67551a2580d
kingvadee <ojeifod8@gmail.com>
2023-12-05 15:07:00 +0100

    setting up production route

--- web_flask/hello_route.py
+++ web_flask/hello_route.py
@@ -3,19 +3,16 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /airbnb-onepage/: Displays 'Hello HBNB!'
+    /: Displays 'Hello HBNB!'.
 """
 from flask import Flask
-
 app = Flask(__name__)
 
-
-@app.route("/airbnb-onepage/", strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
 
